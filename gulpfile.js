ea692bc8c259656f21657aba9a2a2e90b6711fb2
rahul1115 <rahulgupta.ra.com@gmail.com>
2022-03-03 21:36:39 +0530

    'changes'

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const { src, dest, watch } = require("gulp");
+const minifyCss = require("gulp-clean-css");
+const concat = require("gulp-concat");
+const minifyJs = require("gulp-uglify");
+
+const cssfiles = "{fontawsome,splide,sal,odometer,tailwind,main}";
+const jsfiles = "{alpine,splide,sal,odometer,main}";
+
+const buildCss = () => {
+  let csstask = src(`./src/_static/css/${cssfiles}.css`);
+  csstask = csstask.pipe(
+    minifyCss({
+      level: {
+        1: {
+          specialComments: false,
+        },
+      },
+    })
+  );
+  csstask = csstask.pipe(concat("bundle.css"));
+  csstask = csstask.pipe(dest("./public/css"));
+  return csstask;
+};
+
+const buildJs = () => {
+  let jstask = src(`./src/_static/js/${jsfiles}.js`);
+
+  jstask = jstask.pipe(minifyJs());
+  jstask = jstask.pipe(concat("bundle.js"));
+  jstask = jstask.pipe(dest("./public/js"));
+  return jstask;
+};
+
+exports.buildcss = buildCss;
+exports.buildjs = buildJs;
